8989eb15dd763b6759225923cf2a0c7cc14d9fc2
GitC0de <joonyihn@gmail.com>
2024-06-07 13:35:39 +0900

    Modal update 1

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -14,6 +14,7 @@ function Modal({ date, beforeTitle }) {
 
     const closeModal = () => {
         setModal(false);
+        setRevision('');
     }
 
     const ReviseRef = useRef(null);
@@ -40,7 +41,11 @@ function Modal({ date, beforeTitle }) {
             modal 
             ? <div className="modal">
                     <div className="modalInner">
-                        <span className="close" onClick={closeModal}>&times;</span>
+                        <span className="close" onClick={closeModal}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
+                                <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
+                            </svg>
+                        </span>
                         <center>
                         <h2 className="reviseText">
                             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
